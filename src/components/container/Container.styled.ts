dc8867be39ff763a7325a25393d4560f0afbbf44
Breno-Cardozo <breno_cardozogarcia@hotmail.com>
2023-09-20 21:59:51 -0300

    Adição de KeyFrame e Favicon

--- src/components/container/Container.styled.ts
+++ src/components/container/Container.styled.ts
@@ -6,11 +6,27 @@ export const ContainerWrapper = styled("main")(({theme}) => ({
     alignItems: 'center',
     padding: theme.spacing(2),
     margin: theme.spacing(8),
+    animation: 'moveIn .4s ease',
+
+
 
     [theme.breakpoints.down("lg")]:{
         margin: theme.spacing(2,2,10,0),
         padding: theme.spacing(5),
 
+    },
+
+
+
+    "@keyframes moveIn":{
+        "from":{
+            opacity: 0,
+            transform: 'translateY(20px)',
+        },
+        "to":{
+            opacity:1,
+            transform:'translateY(0px)'
+        },
     },
 
 }));
